2e82ec50e4a606776cbb0e0036bb3964b35e3042
Sebastian <gmz.sebastian@gmail.com>
2025-03-18 21:48:09 -0400

    Update License

--- setup.py
+++ setup.py
@@ -7,9 +7,10 @@ setup(
     author_email='sebastian.gomez@austin.utexas.edu',
     description='Functions to determine whether a transient is nuclear.',
     url='https://github.com/gmzsebastian/iinuclear',
-    license='MIT License',
+    license='MIT',
     python_requires='>=3.6',
     packages=['iinuclear'],
+    license_files=["LICENSE"],
     include_package_data=True,
     package_data={'iinuclear': ['ref_data/*']},
     install_requires=[
